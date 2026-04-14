5ec32cf3a68e0f87e271199d8e8a5da663a995fd
arjun <arjunkizhakkelthil07@gmail.com>
2026-04-14 11:37:20 +0530

    calculator added

--- Simple calculator using if-elif (add, subtract, multiply, divide).py
+++ Simple calculator using if-elif (add, subtract, multiply, divide).py
@@ -1,1 +1,12 @@
-d
+n1=int(input("enter the 1st no:"))
+n2=int(input("enter the 2nd number:"))
+
+operator=input("which opeators wants to perform :")
+if operator == '+':
+    print(n1+n2)
+elif operator == '-':
+    print(n1-n2)
+elif operator == '*':
+    print(n1*n2)
+elif operator == '/':
+    print(n1/n2)
