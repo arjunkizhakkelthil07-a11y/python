e35b131cf9f274c2cf8d438f6cc873f50b2d916f
arjun <arjunkizhakkelthil07@gmail.com>
2026-04-14 12:14:16 +0530

    python ticket

---
+++ Movie ticket pricing.py
@@ -0,0 +1,7 @@
+a=int(input("enter your age:"))
+if(a<=12):
+    print("price of ticket is 100" )
+elif(a>60):
+    print("price of the ticket is 150")
+else:
+    print("price of the ticket is 200")
